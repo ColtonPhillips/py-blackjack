15481e47060899eb645d86fca56a6562d5c3e205
Colton Phillips <coltonjphillips@gmail.com>
2013-03-14 20:00:56 -0700

    created some broken code to ask about

--- blackjack.py
+++ blackjack.py
@@ -1,63 +1,47 @@
-PREAMBLE = """
-Welcome to...
+import dialog
 
-Colton Phillips' Blackjack!
+class Card:
+    suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']   
+    def __init__(self):
+        # Whatever. Default to this.
+        self.value = 0
+        self.suit = suits[1]
+        
+    @staticmethod
+    def random_card():
+        import random
+        card = Card()
+        card.value = random.randint(0,13)
+        card.suit = Card.suits[random.randint(0,3)]
+        return card
 
-The coolest console game in the Universe.
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(52):
+            self.cards.append(Card.random_card())
+            print card.value
 
-Here are the rules:
+    def deal_card(self):
+        pass
 
-1) You play it.
-2) You enjoy it. 
-3) You decide whether or not you would like to continue playing.
-
-Any questions? Of course not. Have fun playing Blackjack, kiddo!
-
-"""
-
-EPILOGUE = """
-Well, we've all had some fun. You laughed, you cried. Remember 
-when the blackjack dealer tried to trick you into buying him a
-drink? That was pretty funny. 
-
-I hope you enjoyed this experience. I'm sure you've learned a lot from it.
-
-Cheers,
-C
-
-"""
-
-TABLE_INTRO = """
-You find yourself at a blackjack table. The dealer yells at you:
-
-"Hey! You look like a rube! Prove your worth, you plebian."
-
-"Huh", you think. But it's too late. Thousands of ghost tendrils
-are pulling you in closer to the table. You are certainly going to
-play a game of Blackjack. 
-
-Like it not, that's what's in store for you today.
-
-"""
+class Hand:
+    def __init__(self):
+        pass
 
 def game_loop():
-    print TABLE_INTRO
+    print dialog.TABLE_INTRO
 
     active_game = True
     while active_game: 
-        print """
-        TODO: MAKE A GAME 
-        HAR HAR
-        HAR 
-        
-        """
+        deck = Deck()
         active_game = False
 
 def main():
-    print PREAMBLE
+    print dialog.PREAMBLE
     raw_input("Press Enter Key to play.")
     game_loop()
-    print EPILOGUE
+    print dialog.EPILOGUE
 
 if __name__ == "__main__":
     main()
---
+++ dialog.py
@@ -0,0 +1,31 @@
+
+PREAMBLE = """
+You find yourself at a blackjack table. The dealer yells at you:
+
+"Hey! You look like a rube! Prove your worth, you plebian."
+
+"Huh", you think. But it's too late. Thousands of ghost tendrils
+are pulling you in closer to the table. You are certainly going to
+play a game of Blackjack. 
+
+Like it not, that's what's in store for you today.
+
+"""
+
+TABLE_INTRO = """
+
+Welcome to the table i say something clever before we start playing, now dont I!
+
+"""
+
+EPILOGUE = """
+Well, we've all had some fun. You laughed, you cried. Remember 
+when the blackjack dealer tried to trick you into buying him a
+drink? That was pretty funny. 
+
+I hope you enjoyed this experience. I'm sure you've learned a lot from it.
+
+Cheers,
+C
+
+"""
